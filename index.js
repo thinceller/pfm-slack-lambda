18f90b5997c8cbd0b53688bdd3988bb8ae89ef82
thinceller <kohei.ct@icloud.com>
2019-02-24 14:45:37 +0900

    fix script

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const delay = require('delay');
+const moment = require('moment');
 const puppeteer = require('puppeteer');
 
 const client = require('./slack');
@@ -6,10 +7,11 @@ const client = require('./slack');
 const pfmSigninUrl = 'https://moneyforward.com/users/sign_in';
 const email = process.env.EMAIL
 const password = process.env.PASSWORD
+const name = process.env.YOUR_NAME
 
 exports.handler = async (event, context) => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     executablePath: '/opt/headless-chromium',
     defaultViewport: { width: 1920, height: 1080 },
     args: ['--no-sandbox', '--disable-gpu', '--single-process', '--lang=ja,en-US,en']
@@ -25,20 +27,80 @@ exports.handler = async (event, context) => {
   await Promise.all([
     page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
     page.click('#login-btn-sumit')
-  ])
+  ]);
+
+  await delay(1000);
 
   // 情報取得
-  const title = await page.evaluate(() => {
-    return document.querySelector('title').textContent
-  })
-  await delay(1000);
-  console.log(title);
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+    page.click('a[href="/cf"')
+  ]);
+  await page.click('#in_out button.fc-button-today');
+
+  const dateList = [];
+  const today = moment();
+  // 先週一週間の日付リストを作成
+  for (let i = 0; i < 7; i++) {
+    dateList.push(today.add(-1, 'days').format('YYYY/MM/DD'));
+  }
+
+  const data = await page.evaluate(dateList => {
+    let data = {};
+    const trList = document.querySelectorAll('tr.transaction_list');
+    trList.forEach(node => {
+      const date = node.children[1].dataset.tableSortableValue;
+      if (!dateList.some(targetDate => date.match(targetDate))) return;
+      const amount = Number(node.children[3].querySelector('span').textContent.trim().replace(',', ''));
+      // 収入ならスキップ
+      if (Math.sign(amount) === 1) return;
+      const category = node.children[5].textContent.trim();
+      // 項目なしは振替なのでスキップ
+      if (!category) { return; }
+      data[category] = data[category] || 0;
+      data[category] += amount;
+    });
+    return data;
+  }, dateList);
 
   // Slack通知送信
-  await client.postMessage(title);
+  const message = createSlackMessage(data, name);
+  await client.postMessage(message);
   await delay(1000);
 
   // 終了処理
   await page.close();
   await browser.close();
+}
+
+/**
+ * スクレイピングした結果を受け取って通知用のメッセージを作成する
+ *
+ * @param {Object} data スクレイピング結果
+ * @param {string} name あなたの名前
+ * @returns {string} Slack送信用メッセージ
+ */
+const createSlackMessage = (data, name) => {
+  // dataがからの場合は別のメッセージを返す
+  if (!Object.keys(data).length) {
+    return `
+${name}さん、今週もお疲れ様でした！
+今週の出費はありませんでした。
+`
+  }
+
+  let cost = '';
+  for (let key in data) {
+    cost += `
+${key}: ${data[key] * -1}円
+`
+  }
+  const message = `
+${name}さん、今週もお疲れ様でした！
+今週の出費は以下の通りです⬇️
+
+${cost}
+詳細はアプリからご確認ください🙌
+  `
+  return message
 }
